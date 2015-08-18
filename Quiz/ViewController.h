01c446fc97e71c2f911e634a315d70377ee89c19
Ty Frith <tyfrith@gmail.com>
2015-08-18 16:02:55 -0700

    Initial Commit

---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Ty Frith on 8/18/15.
+//  Copyright (c) 2015 Ty Frith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quiz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Quiz
+//
+//  Created by Ty Frith on 8/18/15.
+//  Copyright (c) 2015 Ty Frith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
